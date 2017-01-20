674fca222581cd96a5f6e83ef80ef362cedd52e2
Kristin Thomas <kmychalt@gmail.com>
2017-01-20 00:09:24 -0800

    Cube class, working on signals

---
+++ cube.py
@@ -0,0 +1,11 @@
+
+
+def default():
+    front = 'R','R','R','R','R','R','R','R','R'
+    back = 'O','O','O','O','O','O','O','O','O'
+    left = 'G','G','G','G','G','G','G','G'
+    right = 'B','B','B','B','B','B','B','B','B'
+    bottom = 'Y','Y','Y','Y','Y','Y','Y','Y','Y'
+    top = 'W','W','W','W','W','W','W','W','W'
+    entireCube = front + back + left + right + bottom + top
+    return entireCube
--- qt_skeleton.py
+++ qt_skeleton.py
@@ -1,15 +1,24 @@
 import sys
+import cube
+from enum import Enum
 from PyQt4 import QtCore, QtGui, uic
+from PyQt4.QtCore import SIGNAL
  
 qtCreatorFile = "rubiks_main.ui" # Enter file here.
  
-Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile) 
  
 class MyApp(QtGui.QMainWindow, Ui_MainWindow):
     def __init__(self):
         QtGui.QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
+        #self.tableWidget.item(1,0).setBackground(QtGui.QColor(125,125,125))
+        #self.connect(self.randomButton, SIGNAL("clicked()"), self.randomButtonClicked)
+        self.connect(self.tableWidget.item(1,0), SIGNAL("clicked()"), self.tableItemClicked)
+        
+    def tableItemClicked(self):
+        Color = Enum('Color', 'red blue white yellow green orange')
  
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
